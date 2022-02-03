58b7e4b9a93a417e4a246379cdf624d83a5a6bf8
the-satyam <satyamvishwa03@gmail.com>
2022-02-03 20:23:19 +0530

    register Activity addition

---
+++ app/src/main/java/com/example/illuminati_store/RegisterActivity.kt
@@ -0,0 +1,32 @@
+package com.example.illuminati_store
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.WindowManager
+
+
+// START
+@Suppress("DEPRECATION")
+class RegisterActivity : AppCompatActivity() {
+
+    /**
+     * This function is auto created by Android when the Activity Class is created.
+     */
+    override fun onCreate(savedInstanceState: Bundle?) {
+        //This call the parent constructor
+        super.onCreate(savedInstanceState)
+        // This is used to align the xml view to this class
+        setContentView(R.layout.activity_register)
+
+
+        // START
+        // This is used to hide the status bar and make the splash screen as a full screen activity.
+        // It is deprecated in the API level 30. I will update you with the alternate solution soon.
+        window.setFlags(
+            WindowManager.LayoutParams.FLAG_FULLSCREEN,
+            WindowManager.LayoutParams.FLAG_FULLSCREEN
+        )
+        // END
+    }
+}
+// END
