acf0581936a9afa08e60503db01c5f056a5e7f56
the-satyam <satyamvishwa03@gmail.com>
2022-02-04 22:40:32 +0530

    register validation

--- app/src/main/java/com/example/illuminati_store/RegisterActivity.kt
+++ app/src/main/java/com/example/illuminati_store/RegisterActivity.kt
@@ -1,13 +1,14 @@
 package com.example.illuminati_store
 
-import androidx.appcompat.app.AppCompatActivity
+
 import android.os.Bundle
+import android.text.TextUtils
 import android.view.WindowManager
+import kotlinx.android.synthetic.main.activity_register.*
 
 
-// START
 @Suppress("DEPRECATION")
-class RegisterActivity : AppCompatActivity() {
+class RegisterActivity : BaseActivity() {
 
     /**
      * This function is auto created by Android when the Activity Class is created.
@@ -18,15 +19,90 @@ class RegisterActivity : AppCompatActivity() {
         // This is used to align the xml view to this class
         setContentView(R.layout.activity_register)
 
-
-        // START
         // This is used to hide the status bar and make the splash screen as a full screen activity.
         // It is deprecated in the API level 30. I will update you with the alternate solution soon.
         window.setFlags(
             WindowManager.LayoutParams.FLAG_FULLSCREEN,
             WindowManager.LayoutParams.FLAG_FULLSCREEN
         )
+
+
+        // START
+        setupActionBar()
+        // END
+
+
+        btn_register.setOnClickListener {
+
+            validateRegisterDetails()
+        }
         // END
     }
+
+
+    /**
+     * A function for actionBar Setup.
+     */
+    private fun setupActionBar() {
+
+        setSupportActionBar(toolbar_register_activity)
+
+        val actionBar = supportActionBar
+        if (actionBar != null) {
+            actionBar.setDisplayHomeAsUpEnabled(true)
+            actionBar.setHomeAsUpIndicator(R.drawable.ic_black_color_back_24dp)
+        }
+
+        toolbar_register_activity.setNavigationOnClickListener { onBackPressed() }
+    }
+    // END
+
+
+    // START
+    /**
+     * A function to validate the entries of a new user.
+     */
+    private fun validateRegisterDetails(): Boolean {
+        return when {
+            TextUtils.isEmpty(et_first_name.text.toString().trim { it <= ' ' }) -> {
+                showErrorSnackBar(resources.getString(R.string.err_msg_enter_first_name), true)
+                false
+            }
+
+            TextUtils.isEmpty(et_last_name.text.toString().trim { it <= ' ' }) -> {
+                showErrorSnackBar(resources.getString(R.string.err_msg_enter_last_name), true)
+                false
+            }
+
+            TextUtils.isEmpty(et_email.text.toString().trim { it <= ' ' }) -> {
+                showErrorSnackBar(resources.getString(R.string.err_msg_enter_email), true)
+                false
+            }
+
+            TextUtils.isEmpty(et_password.text.toString().trim { it <= ' ' }) -> {
+                showErrorSnackBar(resources.getString(R.string.err_msg_enter_password), true)
+                false
+            }
+
+            TextUtils.isEmpty(et_confirm_password.text.toString().trim { it <= ' ' }) -> {
+                showErrorSnackBar(resources.getString(R.string.err_msg_enter_confirm_password), true)
+                false
+            }
+
+            et_password.text.toString().trim { it <= ' ' } != et_confirm_password.text.toString()
+                .trim { it <= ' ' } -> {
+                showErrorSnackBar(resources.getString(R.string.err_msg_password_and_confirm_password_mismatch), true)
+                false
+            }
+            !cb_terms_and_condition.isChecked -> {
+                showErrorSnackBar(resources.getString(R.string.err_msg_agree_terms_and_condition), true)
+                false
+            }
+            else -> {
+                showErrorSnackBar("Your details are valid.", false)
+                true
+            }
+        }
+    }
+    // END
 }
-// END
